07b77e488fd53ad23944258d2aa46ac1cc758aca
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-07-03 21:24:49 -0400

    Added assets
    Added the images for the footer. Added the links for the footer. Added the links and titles for the portfolio. Added profile picture in about.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import './App.css';
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
@@ -39,6 +40,12 @@ function App() {
         <div>
             <NavBar onPageChange={handlePageChange} />
             {renderPage()}
+            <footer>
+                <p>&copy; {new Date().getFullYear()}</p>
+                <a href="https://www.linkedin.com/in/martin-costas-9b6033233/"><img src="./lilogo.png" alt="Linkedin Logo" /></a>
+                <a href="https://github.com/Devnah4"><img src="./GitHubLogo.png" alt="GitHub Logo"/></a>
+                <a href="https://www.youtube.com/user/deathmonkster/featured"><img src="./youtubeicon.png" alt="Youtube Logo"/></a>
+            </footer>
         </div>
     )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,7 +4,19 @@ export default function about() {
     return (
         <div>
             <h1>Projects</h1>
-            
+            <p>I have been working on a number of projects, some of which are listed below:</p>
+            <h1>Budget Tracker</h1>
+            <a href="https://github.com/Devnah4/budget-tracker">View the repository</a>
+            <h1>Social Network Backend Api</h1>
+            <a href="https://github.com/Devnah4/mongodb-social-network-api">View the repository</a>
+            <h1>Food Festival</h1>
+            <a href="https://github.com/Devnah4/food-festival">View the repository</a>
+            <h1>Budge-it</h1>
+            <a href="https://github.com/Devnah4/budge-it">View the repository</a>
+            <h1>Employee Generator</h1>
+            <a href="https://github.com/Devnah4/employee-generator-for-science">View the repository</a>
+            <h1>Employee Tracker</h1>
+            <a href="https://github.com/Devnah4/employee-tracker-for-science">View the repository</a>
         </div>
     )
 }
