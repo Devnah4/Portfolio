c1737d3305713531aef4b7dba134128358d478bd
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-09-25 14:58:11 -0400

    Updated code for footer and base HTML

--- src/App.js
+++ src/App.js
@@ -36,41 +36,28 @@ function App() {
   };
 
   return (
-    <div>
+    <div class="container">
       <NavBar onPageChange={handlePageChange} />
       {renderPage()}
-      <footer class="page-footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12 py-5">
-              <div class="flex-center">
-                <a href="https://www.linkedin.com/in/martin-costas-9b6033233/">
-                  <img
-                    src="./lilogo.png"
-                    alt="Linkedin Logo"
-                    className="logo"
-                  />
-                </a>
-                <a href="https://github.com/Devnah4">
-                  <img
-                    src="./GitHubLogo.png"
-                    alt="GitHub Logo"
-                    className="logo"
-                  />
-                </a>
-                <a href="https://www.youtube.com/user/deathmonkster/featured">
-                  <img
-                    src="./youtubeicon.png"
-                    alt="Youtube Logo"
-                    className="logo"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer-copyright text-center py-3 text-black">
-          &copy; {new Date().getFullYear()}
+      <footer class="container-fluid">
+        <div class="row">
+          <span class="mb-3 mb-md-0 text-muted">&copy; {new Date().getFullYear()}</span>
+          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3">
+              <a href="https://www.linkedin.com/in/martin-costas-9b6033233/">
+                <img src="./lilogo.png" alt="Linkedin Logo" className="logo" />
+              </a>
+            </li>
+            <li class="ms-3">
+              <a href="https://github.com/Devnah4">
+                <img
+                  src="./GitHubLogo.png"
+                  alt="GitHub Logo"
+                  className="logo"
+                />
+              </a>
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
